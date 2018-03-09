a49ba4919b5ebf560b09605999ac5b35aeb9310f
drownedout <danielcdonnelly@gmail.com>
2018-03-08 20:00:26 -0500

    Template, static scaffolding

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 # Root
 @app.route('/')
 def index():
-	pass
+	return render_template('index.html')
 
 # Category Index
 @app.route('/categories')
