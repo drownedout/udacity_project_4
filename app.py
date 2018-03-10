3ab9c57361e87e667bc86ca164738718d1740fcd
drownedout <danielcdonnelly@gmail.com>
2018-03-10 16:12:09 -0500

    Added Category show

--- app.py
+++ app.py
@@ -30,6 +30,13 @@ def categoryIndex():
 	categories = session.query(Category).all()
 	return render_template('categories/index.html', categories=categories)
 
+# Category Show
+@app.route('/categories/<int:category_id>')
+def categoryShow(category_id):
+	category = session.query(Category).filter_by(id=category_id).one()
+	categoryItems = session.query(CategoryItem).filter_by(category_id=category.id).all()
+	return render_template('categories/show.html', category_id = category_id, category=category, categoryItems=categoryItems)
+
 # Item Index
 @app.route('/items')
 def categoryItemIndex():
